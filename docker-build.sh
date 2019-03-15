d48b240b231be33e5a47fa59dce6ca23e32bc9dc
Hanly De Los Santos <lableupunn@gmail.com>
2019-03-14 21:28:44 -0400

    Changed mongodb repository arch to i386.

--- docker-build.sh
+++ docker-build.sh
@@ -37,7 +37,7 @@ apt-get install -qy --no-install-recommends \
     procps \
     libcap2-bin
 tryfail apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "deb [ arch=i386 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 apt-get update
 echo "deb http://www.ubnt.com/downloads/unifi/debian unifi5 ubiquiti" > /etc/apt/sources.list.d/20ubiquiti.list
 tryfail apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv C0A52C50
