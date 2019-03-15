24985988036aaca18583cd4ac99cba1ea3efbc94
Hanly De Los Santos <lableupunn@gmail.com>
2019-03-14 23:16:57 -0400

    Replaced mongodb for a i386 compatible version

--- docker-build.sh
+++ docker-build.sh
@@ -36,13 +36,53 @@ apt-get install -qy --no-install-recommends \
     openjdk-8-jre-headless \
     procps \
     libcap2-bin
-tryfail apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=i386 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+
+curl -O http://downloads.mongodb.org/linux/mongodb-linux-i686-3.2.22.tgz
+tar -zxvf mongodb-linux-i686-3.2.22.tgz
+cp mongodb-linux-i686-3.2.22/bin/* /usr/bin/
+
+curl https://raw.githubusercontent.com/mongodb/mongo/master/debian/init.d > init.d
+mv init.d /etc/init.d/mongod
+chmod 755 /etc/init.d/mongod
+
+cat << MONGO_CONF >> /etc/mongod.conf
+storage:
+  dbPath: /var/lib/mongo
+  journal:
+    enabled: true
+  engine: mmapv1
+systemLog:
+  destination: file
+  logAppend: true
+  path: /var/log/mongodb/mongod.log
+processManagement:
+  fork: true
+net:
+  port: 27017
+  bindIp: 0.0.0.0
+MONGO_CONF
+
+useradd --home-dir /var/lib/mongo --shell /bin/false mongodb
+
+mkdir /var/lib/mongo
+chown -R mongodb /var/lib/mongo
+chgrp -R mongodb /var/lib/mongo
+
+mkdir /var/log/mongodb
+chown -R mongodb /var/log/mongodb
+chgrp -R mongodb /var/log/mongodb
+
+touch /var/run/mongod.pid
+chown mongodb /var/run/mongod.pid
+chgrp mongodb /var/run/mongod.pid
+
+update-rc.d mongod defaults
+
 apt-get update
 echo "deb http://www.ubnt.com/downloads/unifi/debian unifi5 ubiquiti" > /etc/apt/sources.list.d/20ubiquiti.list
 tryfail apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv C0A52C50
 curl -L -o ./unifi.deb "${1}"
-apt -qy install mongodb-org ./unifi.deb
+apt -qy install ./unifi.deb
 rm -f ./unifi.deb
 chown -R unifi:unifi /usr/lib/unifi
 rm -rf /var/lib/apt/lists/*
